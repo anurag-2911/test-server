27f4be8408c132d6b50641a27ea9cfd7c057bd7e
Anurag Kumar <anurag.kumar@microfocus.com>
2022-02-06 01:50:08 +0530

    writing chunks of data in a file.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,53 @@
 
-var express = require('express');
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const req = require("express/lib/request");
 
-app.get('/',(req,res)=>homePage(req,res));
+const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
-function homePage(request,response){
-    // console.log(request);
-    response.send('hello world!');
-}
-
-var server = app.listen(443,function(){
+var server = app.listen(443,() =>{
    var host = server.address().address;
    var port = server.address().port;   
    console.log("Example app listening at http://%s:%s", host, port);
-})
+});
 
-app.get('/zenworks-content/upload/file',(request,response)=>uploadFile(request,response));
+app.post('/zenworks-content/upload/file',(request,response)=>{
+    var data = request.body.toString();
+   
+    var fileName = request.query.fileName;
+    var fileType = request.query.fileType;
+    var totalChunks = request.query.totalChunks;
+    var currentChunk = request.query.currentChunk;
+    var lastModifiedTime = request.query.lastModifiedTime;
+    var overwrite = request.query.overwrite;
 
-function uploadFile(request,response){
-    
-    response.send('hello from upload file');
+    parseRequestData(data,fileName,fileType,totalChunks,currentChunk,lastModifiedTime,overwrite);
+
+    response.sendStatus(200);
+});
+
+function parseRequestData(data,fileName,fileType,totalChunks,currentChunk,
+    lastModifiedTime,overwrite){
+        const fs = require('fs');
+        if(currentChunk==1)
+        {
+            if(fs.existsSync(fileName))
+            {
+                fs.unlinkSync(fileName);
+                
+            }
+        }
+
+        
+        var logStream = fs.createWriteStream(fileName, {flags: 'a'});
+        logStream.write(data);
+        logStream.end();
+
+
 }
+
+
+
